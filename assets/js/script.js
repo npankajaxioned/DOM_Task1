795190cbcd3ea9384e9daf57ee43b2d7d5c36428
npankajaxioned <pankajn@axioned.com>
2022-07-15 19:13:57 +0530

    Changes made in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,25 @@
-
 var categories = document.querySelectorAll('.list');
 var selectedImage = document.querySelectorAll('.tab-image');
 for (var images of selectedImage) {
   images.classList.add('image-active');
 }
 for (i = 0; i < categories.length; i++) {
-  categories[i].addEventListener('click', function () {
+  categories[i].addEventListener('click', tabFilter)
+}
+function tabFilter() {
     for (j = 0; j < categories.length; j++) {
-      categories[j].classList.remove('list-active');
+        categories[j].classList.remove('list-active');
     }
     this.classList.add('list-active');
     var dataList = this.getAttribute('data-list');
+
     for (var images of selectedImage) {
-      images.classList.remove('image-active');
-      if (images.getAttribute('data-name') == dataList || dataList == 'all') {
-        images.classList.add('image-active');
-      }
+        images.classList.remove('image-active');
+        if (images.getAttribute('data-name') == dataList || dataList == 'all') {
+            images.classList.add('image-active');
+        }
     }
-  })
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
