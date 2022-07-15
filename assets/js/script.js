ae2d2fbdabcd3cd38512720064c58a32025a023b
npankajaxioned <pankajn@axioned.com>
2022-07-15 10:37:00 +0530

    Done with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 
-
+var categories = document.querySelectorAll('.list');
+var selectedImage = document.querySelectorAll('.tab-image');
+for (var images of selectedImage) {
+  images.classList.add('image-active');
+}
+for (i = 0; i < categories.length; i++) {
+  categories[i].addEventListener('click', function () {
+    for (j = 0; j < categories.length; j++) {
+      categories[j].classList.remove('list-active');
+    }
+    this.classList.add('list-active');
+    var dataList = this.getAttribute('data-list');
+    for (var images of selectedImage) {
+      images.classList.remove('image-active');
+      if (images.getAttribute('data-name') == dataList || dataList == 'all') {
+        images.classList.add('image-active');
+      }
+    }
+  })
+}
 
 
 
