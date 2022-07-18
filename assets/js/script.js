1f1a54ae694bcc8502b0ce8946b9d2ec35e775df
npankajaxioned <pankajn@axioned.com>
2022-07-18 09:55:30 +0530

    Hamburger Code Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,15 @@ function tabFilter() {
 }
 
 
+/* Hamburger Menu Starts Here */
 
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener("click",() => {
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+})
 
 
 
